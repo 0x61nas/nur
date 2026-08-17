15fdcc82ea0e34b0a19a178d241db6ecd157d1df
0x61nas <anas.elgarhy.dev@gmail.com>
2026-08-17 15:41:46 +0300

    feat: create the autolock service

---
+++ modules/autolock.nix
@@ -0,0 +1,105 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.services.autolock;
+
+  optionalArg = flag: value:
+    lib.optional (value != null && value != "") [
+      flag
+      value
+    ];
+
+  args =
+    lib.concatLists [
+      (optionalArg "-t" cfg.timeout)
+      (optionalArg "-i" cfg.interval)
+      (optionalArg "-c" cfg.lockCommand)
+      (optionalArg "-f" cfg.fullscreenTimeout)
+      (lib.optional cfg.ignoreSleep "--ignore-sleep")
+      (lib.optional cfg.verbose "-v")
+      cfg.extraArgs
+    ];
+in {
+  options.services.autolock = {
+    enable = lib.mkEnableOption "autolock";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.autolock;
+    };
+
+    display = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+    };
+
+    timeout = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Timeout in seconds.";
+    };
+
+    interval = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Interval time in seconds.";
+    };
+
+    lockCommand = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Command to execute when locking.";
+    };
+
+    fullscreenTimeout = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Fullscreen timeout in seconds.";
+    };
+
+    ignoreSleep = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Ignore the sleep signal.";
+    };
+
+    verbose = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    extraArgs = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.autolock = {
+      description = "A minimal X11 idle-watcher";
+
+      wantedBy = [ "graphical.target" ];
+
+      after = [
+        "graphical.target"
+        "display-manager.service"
+      ];
+
+      wants = [
+        "graphical.target"
+      ];
+
+      serviceConfig = {
+        ExecStart = lib.escapeShellArgs (
+          [ "${cfg.package}/bin/autolock" ] ++ args
+        );
+
+        Restart = "on-failure";
+
+        Environment = lib.optional (
+          cfg.display != ""
+        ) "DISPLAY=${cfg.display}";
+      };
+    };
+  };
+}
